4de206088db4e5b0d5b9397c9d71d3566a880ecf
Kristóf Rácz <racz.kristof26@gmail.com>
2021-04-12 18:09:07 +0200

    Console and plot base
    Base VS C# project to use both a console interface and a form window for a plot.

---
+++ nn-xor-demo-cs/nn-xor-demo-cs/Plot.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace nn_xor_demo_cs
+{
+    public partial class Plot : Form
+    {
+        public Plot()
+        {
+            InitializeComponent();
+
+            Console.WriteLine("Hello World!");
+
+            string cmd = Console.ReadLine();
+
+            if (cmd == "plot")
+                DrawTestPlot();
+        }
+
+        private void DrawTestPlot()
+        {
+            chart1.Series.Clear();
+
+            chart1.Titles.Add("Total Income");
+
+            Series series = chart1.Series.Add("Total Income");
+            series.ChartType = SeriesChartType.Spline;
+            series.Points.AddXY("September", 100);
+            series.Points.AddXY("Obtober", 300);
+            series.Points.AddXY("November", 800);
+            series.Points.AddXY("December", 200);
+            series.Points.AddXY("January", 600);
+            series.Points.AddXY("February", 400);
+        }
+    }
+}
---
+++ nn-xor-demo-cs/nn-xor-demo-cs/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("nn-xor-demo-cs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("nn-xor-demo-cs")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9406caef-62d5-4c7c-baf5-9b9732a09d30")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
