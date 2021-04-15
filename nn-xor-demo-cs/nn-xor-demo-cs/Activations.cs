25193d7bf6a3e83926bf3e0f46d79666bc1f6cbb
Kristóf Rácz <racz.kristof26@gmail.com>
2021-04-15 17:11:30 +0200

    Activation function basics
    Implementation of the Unity activation function using LINQ. This is an inefficient method for Unity, but represents the nature of an activation function better, and will serve as a basis for all further activation functions.

---
+++ nn-xor-demo-cs/nn-xor-demo-cs/Activations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nn_xor_demo_cs
+{
+    public static class Activations
+    {
+        // Unity activation function. The implementation is inefficient, but represents
+        // the nature of an activation function better then "return input". Shows the
+        // general implementation scheme for using LINQ to perform the calcualtion on
+        // all elemnts of the array.
+        public static double[] Unity(this double[] input, bool deriv = false)
+        {
+            double[] output;
+
+            if (deriv)
+                output = (from value in input
+                          select value).ToArray();
+            else
+                output = (from value in input
+                          select 1.0).ToArray();
+
+            return output;
+        }
+    }
+}
