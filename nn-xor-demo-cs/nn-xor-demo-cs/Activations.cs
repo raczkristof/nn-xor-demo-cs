b9c55f4a24b7511e62f0fde1fccfa8d2d4aefdc0
Kristóf Rácz <racz.kristof26@gmail.com>
2021-04-15 22:03:35 +0200

    Activation functions
    Implemented the sigmoid, tanH, ReLU and leakyReLU activation functions and their derivates.

--- nn-xor-demo-cs/nn-xor-demo-cs/Activations.cs
+++ nn-xor-demo-cs/nn-xor-demo-cs/Activations.cs
@@ -12,16 +12,88 @@ namespace nn_xor_demo_cs
         // the nature of an activation function better then "return input". Shows the
         // general implementation scheme for using LINQ to perform the calcualtion on
         // all elemnts of the array.
-        public static double[] Unity(this double[] input, bool deriv = false)
+        public static double[] Identity(this double[] input, bool deriv = false)
         {
             double[] output;
 
             if (deriv)
-                output = (from value in input
+                output = (from x in input
                           select 1.0).ToArray();
             else
-                output = (from value in input
-                          select value).ToArray();
+                output = (from x in input
+                          select x).ToArray();
+
+            return output;
+        }
+
+        public static double[] Sigmoid(this double[] input, bool deriv = false)
+        {
+            double[] output;
+
+            if (deriv)
+                output = (from x in input
+                          select Math.Exp(-x) / Math.Pow(1 + Math.Exp(-x), 2)).ToArray();
+            else
+                output = (from x in input
+                          select 1 / (1 + Math.Exp(-x))).ToArray();
+
+            return output;
+        }
+
+        public static double[] TanH(this double[] input, bool deriv = false)
+        {
+            double[] output;
+
+            if (deriv)
+                output = (from x in input
+                          select 1 - Math.Tanh(x) * Math.Tanh(x)).ToArray();
+            else
+                output = (from x in input
+                          select Math.Tanh(x)).ToArray();
+
+            return output;
+        }
+
+        public static double[] ReLU(this double[] input, bool deriv = false)
+        {
+            double[] output = new double[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] < 0) output[i] = 0;
+                else
+                {
+                    if (deriv)
+                        output[i] = 1.0;
+                    else
+                        output[i] = input[i];
+                }
+            }
+
+            return output;
+        }
+
+        public static double[] LReLU(this double[] input, double alpha, bool deriv = false)
+        {
+            double[] output = new double[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] < 0)
+                {
+                    if (deriv)
+                        output[i] = alpha;
+                    else
+                        output[i] = alpha * input[i];
+                }
+                else
+                {
+                    if (deriv)
+                        output[i] = 1.0;
+                    else
+                        output[i] = input[i];
+                }
+            }
 
             return output;
         }
